305eb7c3173eddf79c92dd3d6574eea43cf37aec
LegionDev370 <llegion630@gmail.com>
2022-08-05 22:39:56 +0500

    catalog page add

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// let a = 24
+// let b = 100
+// console.log(a + b);
+
+// let d = 100
+// let c = 200
+// let summ = d + c
+// if (summ >= 600){
+//     console.log("yeah");
+// }
+// else{
+//     console.log("ooops");
+// }
+// prompt("Enter your username")
+
+// let number = Number(prompt("Enter number"))
+// let user = prompt("Enter your username")
+// console.log(String(number) + String(user));
+
+// const num1 = Number(prompt("Enter first number"))
+// const num2 = Number(prompt("Enter second number"))
+// function myfunction(num1,num2){
+//     return num1 + num2
+// }
+// console.log("Result calculation = " + myfunction(num1,num2));
+
+// let x = myfunction(4,5)
+// function myfunction(a,b){
+//     return a * b
+// }
+// console.log(x);
+
+// let summ = prompt("Enter username")
+// console.log("Hi!!!" + summ)
+
+// let number = 20
+// let second = 30
+// const result = number + second
+// let user = Number(prompt("Enter number"))
+// if (result != user){
+//     console.log("Great");
+// }else{
+//     console.log("OOps");
+// }
+
+// const euro = 11650
+// const dollar = 10650
+// let tourst = 200
+// let transp = 300
+// const eurotosumm = euro * tourst
+// const dollartosumm = transp * dollar
+// const result = eurotosumm + dollartosumm
+// let user = Number(prompt("Enter number"))
+// if (user >= result){
+//     console.log("Your money full");
+// }else if(user > result){
+//     console.log("Your great");
+// }else{
+//     console.log("Oops your money not enough");
+// }
+
+// var a = carName = "Ferarri"; surName = "Kamolov"
+// console.log(a);
+// let day;
+// switch (new Date().getDay){
+//     case 0:
+//         day = "Sunday";
+//         break
+// }
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
